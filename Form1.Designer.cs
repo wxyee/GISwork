fdf06657446d7f6083207adb1d9671427166cc3a
24287 <24287@LAPTOP-APFCLQTF>
2022-09-05 20:09:07 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.axSuperWorkspace1 = new AxSuperMapLib.AxSuperWorkspace();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.axSuperWkspManager1 = new AxSuperWkspManagerLib.AxSuperWkspManager();
+            this.axSuperTopo1 = new AxSuperTopoLib.AxSuperTopo();
+            this.axSuperMap1 = new AxSuperMapLib.AxSuperMap();
+            this.axSuperLegend1 = new AxSuperLegendLib.AxSuperLegend();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperWorkspace1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperWkspManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperTopo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperMap1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperLegend1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开数据集";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axSuperWorkspace1
+            // 
+            this.axSuperWorkspace1.Enabled = true;
+            this.axSuperWorkspace1.Location = new System.Drawing.Point(723, 294);
+            this.axSuperWorkspace1.Name = "axSuperWorkspace1";
+            this.axSuperWorkspace1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSuperWorkspace1.OcxState")));
+            this.axSuperWorkspace1.Size = new System.Drawing.Size(32, 32);
+            this.axSuperWorkspace1.TabIndex = 1;
+            this.axSuperWorkspace1.Enter += new System.EventHandler(this.axSuperWorkspace1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(418, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "打开工作空间";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(418, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "关闭工作空间";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // axSuperWkspManager1
+            // 
+            this.axSuperWkspManager1.Enabled = true;
+            this.axSuperWkspManager1.Location = new System.Drawing.Point(12, 1);
+            this.axSuperWkspManager1.Name = "axSuperWkspManager1";
+            this.axSuperWkspManager1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSuperWkspManager1.OcxState")));
+            this.axSuperWkspManager1.Size = new System.Drawing.Size(385, 287);
+            this.axSuperWkspManager1.TabIndex = 4;
+            this.axSuperWkspManager1.LClick += new AxSuperWkspManagerLib._DSuperWkspManagerEvents_LClickEventHandler(this.axSuperWkspManager1_LClick);
+            // 
+            // axSuperTopo1
+            // 
+            this.axSuperTopo1.Enabled = true;
+            this.axSuperTopo1.Location = new System.Drawing.Point(670, 294);
+            this.axSuperTopo1.Name = "axSuperTopo1";
+            this.axSuperTopo1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSuperTopo1.OcxState")));
+            this.axSuperTopo1.Size = new System.Drawing.Size(32, 32);
+            this.axSuperTopo1.TabIndex = 5;
+            // 
+            // axSuperMap1
+            // 
+            this.axSuperMap1.Enabled = true;
+            this.axSuperMap1.Location = new System.Drawing.Point(12, 294);
+            this.axSuperMap1.Name = "axSuperMap1";
+            this.axSuperMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSuperMap1.OcxState")));
+            this.axSuperMap1.Size = new System.Drawing.Size(303, 167);
+            this.axSuperMap1.TabIndex = 6;
+            // 
+            // axSuperLegend1
+            // 
+            this.axSuperLegend1.Enabled = true;
+            this.axSuperLegend1.Location = new System.Drawing.Point(321, 294);
+            this.axSuperLegend1.Name = "axSuperLegend1";
+            this.axSuperLegend1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axSuperLegend1.OcxState")));
+            this.axSuperLegend1.Size = new System.Drawing.Size(304, 167);
+            this.axSuperLegend1.TabIndex = 7;
+            this.axSuperLegend1.Modified += new System.EventHandler(this.axSuperLegend1_Modified);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 473);
+            this.Controls.Add(this.axSuperLegend1);
+            this.Controls.Add(this.axSuperMap1);
+            this.Controls.Add(this.axSuperTopo1);
+            this.Controls.Add(this.axSuperWkspManager1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axSuperWorkspace1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperWorkspace1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperWkspManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperTopo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperMap1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axSuperLegend1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private AxSuperMapLib.AxSuperWorkspace axSuperWorkspace1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxSuperWkspManagerLib.AxSuperWkspManager axSuperWkspManager1;
+        private AxSuperTopoLib.AxSuperTopo axSuperTopo1;
+        private AxSuperMapLib.AxSuperMap axSuperMap1;
+        private AxSuperLegendLib.AxSuperLegend axSuperLegend1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using SuperMapLib;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//打开数据集
+        {
+            if (this.openFileDialog1.ShowDialog()== DialogResult.OK)
+            {
+                object objWSHandle = this.axSuperWorkspace1.CtlHandle;//和workspace控件的关联
+                this.axSuperWorkspace1.Open(this.openFileDialog1.FileName);//打开工作空间的文件名
+                soDataSources soDss = this.axSuperWorkspace1.Datasources;//获得数据源集合
+               
+                soDatasets soDtss = soDss[1].Datasets;
+               
+                soLayers soLys = axSuperMap1.Layers; //获得地图窗口的图层集合
+
+                soLayer objLy = soLys.AddDataset(soDtss[1], true);//添加指定数据集到地图窗口中
+                this.axSuperMap1.ViewEntire(); //地图窗口全幅显示
+                axSuperMap1.Refresh(); //刷新地图窗口
+                axSuperLegend1.Refresh();//刷新图例窗口
+            }
+        }
+
+        private void axSuperWorkspace1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//打开工作空间
+        {
+            bool blnOpen = false;
+            string strOpenPath = string.Empty;
+            this.openFileDialog1.Title = "打开工作空间";
+            this.openFileDialog1.FileName = "";
+            this.openFileDialog1.Filter = "工作空间(.smw)|*.smw";//设置对话框打开文件的类型
+            this.openFileDialog1.InitialDirectory = "..\\..\\..\\Data\\";//对话框打开时的初始文件路径
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                strOpenPath = this.openFileDialog1.FileName;
+                object objWSHandle = this.axSuperWorkspace1.CtlHandle;
+
+                blnOpen = this.axSuperWorkspace1.Open(strOpenPath, "");
+                if (!blnOpen)
+                {
+                    MessageBox.Show("打开工作空间失败");
+                    return;
+                }
+                else
+                {
+                  
+                    soSelection objSelection = axSuperMap1.selection;//获得选择集
+                    soStyle objStyle = objSelection.Style; //选择集风格
+                    objStyle.BrushStyle = 0; //填充风格，取值为填充库中的编码
+                    objStyle.PenStyle = 0; //线型，取值为线型的编码
+                    objStyle.BrushColor = System.Convert.ToUInt32(System.Drawing.ColorTranslator.ToOle(Color.FromArgb(255, 190, 189))); //填充颜色
+                    objStyle.PenColor = System.Convert.ToUInt32(System.Drawing.ColorTranslator.ToOle(Color.FromArgb(0, 0, 255)));//线颜色
+
+                   //ReleaseObjects(objStyle);
+                   // ReleaseObjects(objSelection);
+                    //刷新工作空间管理器，将打开的工作空间可视化在工作空间管理器中
+                    this.axSuperWkspManager1.Refresh();
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)//关闭工作空间
+        {
+            axSuperWkspManager1.Disconnect();
+            axSuperLegend1.Disconnect();
+            axSuperMap1.Disconnect();
+            axSuperMap1.Close();
+            axSuperWorkspace1.Close();
+            axSuperMap1.Refresh();
+        }
+
+        private void axSuperMap1_GeometrySelected(object sender, AxSuperMapLib._DSuperMapEvents_GeometrySelectedEvent e)
+        {
+
+        }
+
+        private void axSuperWkspManager1_LClick(object sender, AxSuperWkspManagerLib._DSuperWkspManagerEvents_LClickEvent e)
+        {
+
+        }
+
+        private void axSuperLegend1_Modified(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
